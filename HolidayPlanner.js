8e13766a75cae3ead6ac59e2746fa801a2261ac7
Samu Tiainen <samu.tiainen@hotmail.com>
2021-05-12 21:37:01 +0300

    MAin code is ready_2

--- HolidayPlanner.js
+++ HolidayPlanner.js
@@ -1,43 +1,69 @@
 export default class HolidayPlanner{
-    constructor(nationalHolidays){
-        this.nationalHolidays = nationalHolidays;
+    constructor(nationalHolidays, holidayPeriodStart, holidayPeriodEnd, maxHolidaySpan){
+        this.nationalHolidays = nationalHolidays.map(day => day.getTime());
+        this.holidayPeriodStart = holidayPeriodStart;
+        this.holidayPeriodEnd = holidayPeriodEnd;
+        this.maxHolidaySpan = maxHolidaySpan;
     }
 
     calculateUsedHolidays(timespan){
         const {startDate, endDate, error} = parseTimespanInputString(timespan);
         if(error) return error;
+        if(!this.isDateInHolidayperiod(startDate) || !this.isDateInHolidayperiod(endDate)) return "Date span not in holiday period";
         const passedDays = getPassedDays(startDate, endDate);
+        if(passedDays > this.maxHolidaySpan) return `Planned holiday ${passedDays}days exceeds the max holiday span ${this.maxHolidaySpan}days` ;
+        const bonusHolidays = calculateBonusHolidaysAndSundays({startDate, endDate}, this.nationalHolidays); 
+        const totalHolidayDaysNeeded = passedDays - bonusHolidays;
+        return `Total holiday days needed is ${totalHolidayDaysNeeded}`;
+    }
 
-        console.log(passedDays);
+    isDateInHolidayperiod = (date) => {
+        return (date >= this.holidayPeriodStart || date <= this.holidayPeriodEnd);
     }
 }
+
+const calculateBonusHolidaysAndSundays = ({startDate, endDate}, bonusDates) =>{
+    let bonusHoliday = 0;
+    //Loop trough datespan and check if the day is sunday (0)
+    for (var i = startDate; i <= endDate; i.setDate(i.getDate()+1)){
+        if (i.getDay() == 0 || bonusDates.includes(i.getTime())) bonusHoliday++;
+    }
+    return bonusHoliday;
+}
+
 
 const parseTimespanInputString = (timespanString) => {
     if(typeof timespanString !== "string") return null;
     //Split two dates from input string
-    let [startDateString, endDateString] = timespanString.split("-");
-    //Remove empty spaces
-    startDateString = startDateString.replace(/\s/g, '');
-    endDateString = endDateString.replace(/\s/g, '');
-    //Get day, month, year from string
-    const [startDay, startMonth, startYear] = startDateString.split(".");
-    const [endDay, endMonth, endYear] = endDateString.split(".");
-    //Create date object
-    const startDate = new Date(startYear, startMonth, startDay);
-    const endDate = new Date(endYear, endMonth, endDay);
+    const dateeSpanStrings = timespanString.split("-");
+    if(dateeSpanStrings.length !== 2) return {error: "date was not given in required format (1.1.2021)"};;
+    let [startDateString, endDateString] = dateeSpanStrings;
+    const startDate = getDateFromDateString(startDateString);
+    const endDate = getDateFromDateString(endDateString);
     //Check if date object created succeesfully
     if(!isValidDate(startDate) || !isValidDate(endDate)){
-        return {error: "date was not consistent of numbers"};;
+        return {error: "date was not given in required format (1.1.2021)"};
     }
-    //Check if date is changed after creating it, if days ggiven were more than there is in that current month
-    if(startDate.getDate() != startDay || (startDate.getMonth()) != startMonth
-    || endDate.getDate() != endDay || (endDate.getMonth()) != endMonth
-    ) return {error: "days or months were too big"};
     return {startDate, endDate};
 }
 
+const getDateFromDateString = (dateString) => {
+    //Remove empty spaces
+    dateString = dateString.replace(/\s/g, '');
+    //Get day, month, year from string
+    const dateValues = dateString.split(".");
+    // if there is multiple numbers
+    if(dateValues.length !== 3) return;
+    const [Day, Month, Year] = dateValues;
+    const date = new Date(Year, Month, Day);
+    //Check if date is changed after creating it, if days ggiven were more than there is in that current month
+    if(date.getDate() != Day || (date.getMonth()) != Month)return;
+    //Create and return date object, No need for checks cos Date creation never fails
+    return date;
+}
+
 const isValidDate = (date) => {
-    return date instanceof Date && !isNaN(date);
+    return date && date instanceof Date && !isNaN(date);
 }
 
 const getPassedDays = (startDate, endDate) => {
--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 import HolidayPlanner from "./HolidayPlanner.js";
 
-const nationalHolidays = [new Date(2020, 1, 1)];
+const nationalHolidays = [
+    new Date(2020, 1, 1),
+    new Date(2020, 1, 6),
+    new Date(2020, 4, 10),
+    new Date(2020, 4, 13),
+    new Date(2020, 5, 1),
+    new Date(2020, 5, 21),
+    new Date(2020, 6, 19),
+    new Date(2020, 12, 24),
+    new Date(2020, 12, 25),
+    new Date(2021, 1, 1),
+    new Date(2021, 1, 6),
+    new Date(2021, 4, 2),
+    new Date(2020, 4, 5),
+    new Date(2020, 20, 6),
+    new Date(2020, 12, 6),
+    new Date(2020, 12, 24),
+];
+const periodStart = new Date(2020, 4, 1);
+const periodEnd = new Date(2021, 3, 1);
+const maxHolidaySpan = 50;
 
-const planner = new HolidayPlanner(nationalHolidays);
+const planner = new HolidayPlanner(nationalHolidays, periodStart, periodEnd, maxHolidaySpan);
 
 console.log(planner.calculateUsedHolidays("1.7.2020 - 11.7.2020"));
